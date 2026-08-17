3ad28550b8adebd66640218cbeb7c7619dc30a57
mtfrs-dev <markus.togi@gmail.com>
2026-08-17 16:19:35 +0700

    refactor: remove healthchecks from web guard

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::get('/healthchecks', [HealthController::class, 'check'])->name('healthchecks');
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
